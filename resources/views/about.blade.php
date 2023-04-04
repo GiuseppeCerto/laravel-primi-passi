9cee3ae3c03704c9544a53024baebfa8548f86dd
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-04-04 16:38:30 +0200

    EDIT new routes and new views

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel Primi Passi</title>
+    </head>
+    <body>
+        <h1>Some information about our business.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec condimentum aliquam euismod. Nunc erat mi, rhoncus ut imperdiet nec, porta vitae velit. Mauris porta nisl eget odio fermentum, in pellentesque ex convallis. Nunc varius fringilla est nec bibendum. In porta, nulla non venenatis mattis, enim orci accumsan quam, ac convallis mi lorem sed diam. Ut at eleifend orci. Morbi convallis semper congue. Proin facilisis lorem vitae posuere pulvinar. Proin id luctus velit. Curabitur ultricies semper varius. Nulla eget hendrerit arcu. Curabitur id diam vel turpis rhoncus hendrerit nec a magna.</p>
+    </body>
+</html>
---
+++ resources/views/services.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel Primi Passi</title>
+    </head>
+    <body>
+        <h1>Our services</h1>
+        <ul>
+            <li>Lorem</li>
+            <li>Ipsum</li>
+            <li>Dolor</li>
+            <li>Sim Amet</li>
+        </ul>
+    </body>
+</html>
